87311733e1cb0f8351707156818311ed1e7e9ed3
Kuldeep Singh <kuldeep.img123@gmail.com>
2023-05-22 16:04:50 +0530

    update user schema

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,18 +1,50 @@
 const mongoose = require('mongoose');
-
-
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    userName:{type:String},
-    email:{type:String, unique:true},
-    password:{type:String, required: true},
-    mobile:{type:Number, unique: true},
-    profilePhoto:
-    {
-        filename: String,
-        path: String
+  userName: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String, required: true },
+  mobile: { type: Number, unique: true },
+  profilePhoto:
+  {
+    filename: String,
+    path: String
+  },
+  cart: [{
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1
     }
+  }],
+  address: [{
+    street: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,
+      required: true
+    },
+    postalCode: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
 
